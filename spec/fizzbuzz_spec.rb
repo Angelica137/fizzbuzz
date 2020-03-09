71e78638f4853f774706f708a0dd1a6d454ef5a3
Angelica <Angelica137@users.noreply.github.com>
2020-03-09 15:40:07 +0000

    add tests for multiples of 3 and resolve

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,5 +8,9 @@ describe "Fizzbuzz" do
   it 'retunrs 4 when passed 4' do
     expect(fizzbuzz(4)). to eq 4
   end
+  it 'returns fizz when passed some multiples of 3' do
+    expect(fizzbuzz(6)). to eq 'fizz'
+    expect(fizzbuzz(999)). to eq 'fizz'
+  end
 
 end
